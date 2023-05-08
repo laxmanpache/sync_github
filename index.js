f4bf7854609ef1809b7db5277637debee4759702
LaxmanPache123 <laxman.p@solytics-partners.com>
2023-05-08 08:48:30 +0530

    access token added for authentication

--- index.js
+++ index.js
@@ -5,11 +5,11 @@
       repo: 'noteapp',
       branch: 'main'
     },
-    // {
-    //   owner: 'Nuva-Org',
-    //   repo: 'model_inventory',
-    //   branch: 'v1.0.0'
-    // },
+    {
+      owner: 'LaxmanPache123',
+      repo: 'funny',
+      branch: 'main'
+    },
     {
         owner: 'Nuva-Org',
         repo: 'model_inventory',
@@ -27,47 +27,23 @@
       // },
   ];
 
-// const https = require('https');
 import * as fs from 'fs'
-import * as https from 'https'
-// const fetch = require('node-fetch');
-// const fetch = require('./fetch');
-import fetch from 'node-fetch';
-// for (const repo of repositories) {
-//     const zipUrl = `https://github.com/${repo.owner}/${repo.repo}/archive/${repo.branch}.zip`;
-//     const zipFile = `./zipFiles/${repo.owner}-${repo.repo}-${repo.branch}.zip`;
-//     const file = fs.createWriteStream(zipFile);
-//     https.get(zipUrl, function(response) {
-//       console.log(zipUrl)
-//       console.log('statusCode:', response.statusCode);
-//       response.pipe(file);
-//       // response.on('data', (d) => {
-//       //   console.log(d)
-//       //   process.stdout.write(d);
-//       // });
-    
-//     }).on('error', (e) => {
-//       console.error(e);
-//     });
-//   }
+import fetch from 'node-fetch'
+const token='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'
 const getAllZip=async()=>{
   console.log('process started')
   for(const repo of repositories){
-    const zipUrl = `https://github.com/${repo.owner}/${repo.repo}/archive/${repo.branch}.zip`;
+    const zipUrl = `https://api.github.com/repos/${repo.owner}/${repo.repo}/zipball/${repo.branch}`;
       const zipFile = `./zipFiles/${repo.owner}-${repo.repo}-${repo.branch}.zip`;
-    const response = await fetch(zipUrl);
-    // const file = fs.createWriteStream(zipFile);
-    // https.get(zipUrl,(res)=>{
-    //   // console.log(res)
-    //   // console.log(res)
-    //   res.pipe(file)
-
-    // })
-    // console.log(response)
-  console.log(zipUrl)
+    const response = await fetch(zipUrl, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
   if (response.ok) {
     const dest = fs.createWriteStream(zipFile);
     response.body.pipe(dest);
+    console.log('file downloaded')
   } else {
     console.log('Failed to download file:', response.statusText);
   }
